cdd8e95c6f2a96f6be2d1fcaa236d191d3f97cb0
Chris Loew <cloew123@gmail.com>
2013-03-04 09:00:03 -0700

    Pointed db_generator at a local database

--- src/db_generator.py
+++ src/db_generator.py
@@ -5,7 +5,7 @@ from ORM.orm_base import Base
 
 def GenerateDatabase():
     """ Generate the Database """
-    engine = create_engine('sqlite:///:memory:', echo=True)
+    engine = create_engine('sqlite:///db/pas.db', echo=True)
     Base.metadata.create_all(engine)
 
 if __name__ == "__main__":
