d8b1a536d7bb28199c76087b30f918dfef4d7b32
Chris Loew <cloew123@gmail.com>
2013-03-04 10:19:55 -0700

    Added logic to test new Transaction Interface

--- src/main.py
+++ src/main.py
@@ -3,32 +3,34 @@ from ORM.transaction import Transaction
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+import datetime
+
 def main():
     """ Generate the Database """
     engine = create_engine('sqlite:///db/pas.db', echo=True)
 
-    my_transaction = Transaction('Some Transaction')
-    print "Name:", my_transaction.name
+    my_transaction = Transaction(description="Dinner", amount=251.00*100, income=False, date=datetime.datetime.now())
+    print my_transaction
     print "ID:", str(my_transaction.id)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
     session.add(my_transaction)
-    saved_transaction = session.query(Transaction).filter_by(name='Some Transaction').first()
-    print "Name:", saved_transaction.name
+    saved_transaction = session.query(Transaction).filter_by(description="Dinner").first()
+    print saved_transaction
     print "ID:", str(saved_transaction.id)
 
     print "Same?:", saved_transaction is my_transaction
 
-    my_transaction.name = "New Transaction Name"
+    my_transaction.description = "New Description"
 
     print session.dirty
 
     session.commit()
 
-    saved_transaction = session.query(Transaction).filter_by(name='New Transaction Name').first()
-    print "Name:", saved_transaction.name
+    saved_transaction = session.query(Transaction).filter_by(description="New Description").first()
+    print saved_transaction
     print "ID:", str(saved_transaction.id)
 
     print "Same?:", saved_transaction is my_transaction
