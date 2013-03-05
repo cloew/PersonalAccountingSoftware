68c2ec922cfd0141c8690848aa229c6af2149c9c
Chris Loew <cloew123@gmail.com>
2013-03-05 10:46:39 -0700

    Cleaned up imports in the Transaction Table Model

--- src/Qt/Model/transaction_table_model.py
+++ src/Qt/Model/transaction_table_model.py
@@ -1,13 +1,13 @@
-from PyQt4 import QtCore
+from PyQt4.QtCore import QAbstractTableModel, QVariant, Qt
 
 from db.transactions import Transactions
 
-class TransactionTableModel(QtCore.QAbstractTableModel):
+class TransactionTableModel(QAbstractTableModel):
     """ Represnts the Transaction List as a Table """
 
     def __init__(self):
         """ Build the Transactions Table """
-        QtCore.QAbstractTableModel.__init__(self)
+        QAbstractTableModel.__init__(self)
 
     def rowCount(self, parent):
         """ Returns the number of rows in the table """
@@ -17,10 +17,10 @@ class TransactionTableModel(QtCore.QAbstractTableModel):
         """ Returns the number of columns in the table """
         return 4
 
-    def data(self, index, role = QtCore.Qt.DisplayRole):
+    def data(self, index, role = Qt.DisplayRole):
         """ Return the data at the given index """
         if not index.isValid():
-            return QtCore.QVariant("Some Value")
+            return QVariant("Some Value")
         else:
             return self.getDataBasedonRole(index, role)
 
@@ -28,23 +28,23 @@ class TransactionTableModel(QtCore.QAbstractTableModel):
         """ Get Data based on the role given """
         print "Getting Data Based on Role:", role
 
-        if role == QtCore.Qt.DisplayRole:
+        if role == Qt.DisplayRole:
             return self.getDisplayRoleData(index)
-        elif role == QtCore.Qt.TextAlignmentRole:
+        elif role == Qt.TextAlignmentRole:
             return self.getTextAlignmentRole(index)
-        return QtCore.QVariant("")
+        return QVariant("")
 
     def getDisplayRoleData(self, index):
         """ Return Data for the Qt Display Role """
-        return QtCore.QVariant("Some Value")
+        return QVariant("Some Value")
 
     def getTextAlignmentRole(self, index):
         """ Return Text Alignment for the given cell """
-        return int(QtCore.Qt.AlignRight|QtCore.Qt.AlignVCenter)
+        return int(Qt.AlignLeft|Qt.AlignVCenter)
 
-    def headerData(self, section, orientation, role = QtCore.Qt.DisplayRole):
+    def headerData(self, section, orientation, role = Qt.DisplayRole):
         """ Return Header Data """
-        if QtCore.Qt.Horizontal == orientation:
-            return QtCore.QVariant("Horizontal Header")
+        if Qt.Horizontal == orientation:
+            return QVariant("Horizontal Header")
         else:
-            return QtCore.QVariant("Vertical Header")
+            return QVariant("Vertical Header")
