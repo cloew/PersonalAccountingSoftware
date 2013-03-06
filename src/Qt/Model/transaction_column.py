6fbe2792b9c17cca86c129ab391ece4d1296d4ef
Chris Loew <cloew123@gmail.com>
2013-03-05 22:02:59 -0700

    Added a base class to handle some common functionality between all the Transaction Columns

---
+++ src/Qt/Model/transaction_column.py
@@ -0,0 +1,30 @@
+from db.transactions import Transactions
+from PyQt4.QtCore import QAbstractTableModel, QModelIndex, QVariant, Qt
+
+class TransactionColumn:
+    """ Represents a column in the Transaction Table """
+    header_name = "TODO: Replace with real name" # Should be overridden in Sub Class
+
+    def getHorizontalHeader(self):
+        """ Return Header Data """
+        return QVariant(self.header_name)
+
+    def getData(self, row):
+        """ Return data for the provided row.
+            Should be overridden in sub class """
+
+    def setData(self, row, value):
+        """ Set data for the provided row.
+            Return whether the entry was set.
+            Should be overridden in sub class. """
+
+    def getTip(self, row):
+        """ Return the Status/Tool Tip for the given row.
+            Should be overridden in sub class. """
+
+    def getTransactionForRow(self, index):
+        """ Returns the Transaction in the given row """
+        row = index.row()
+        transactions = Transactions.all()
+        if row < len(transactions):
+            return transactions[row]
