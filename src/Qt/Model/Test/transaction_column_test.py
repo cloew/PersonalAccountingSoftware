d5f9946c3e5e143b6d90cfa3d51a9b0158cc699f
Chris Loew <cloew123@gmail.com>
2013-03-06 12:34:39 -0700

    Added test for getTransactionForRow

--- src/Qt/Model/Test/transaction_column_test.py
+++ src/Qt/Model/Test/transaction_column_test.py
@@ -1,3 +1,4 @@
+from db.transactions import Transactions
 from Qt.Model.transaction_column import TransactionColumn
 
 import unittest
@@ -19,8 +20,33 @@ suiteGetHorizontalHeader = unittest.TestSuite(map(getHorizontalHeader, testcases
 
 ##########################################################
 
+class getTransactionForRow(unittest.TestCase):
+    """ Test cases of getTransactionForRow """
+    
+    def  setUp(self):
+        """ Build the *** for the test """
+        self.transactionColumn = TransactionColumn()
+        
+    def validRow(self):
+        """ Test that a- proper Transaction is returned when a valid row is provided """
+        assert len(Transactions.all()) > 0, "Should have a transaction"
+        row = len(Transactions.all()) - 1
+        assert Transactions.all()[row] == self.transactionColumn.getTransactionForRow(row), "Should receive the Transaction in the given row"
+
+    def invalidRow(self):
+        """ Test that None is returned when a valid row is provided """
+        assert len(Transactions.all()) > 0, "Should have a transaction"
+        row = len(Transactions.all())
+        assert self.transactionColumn.getTransactionForRow(row) is None, "Should receive None when the row is too high"
+
+# Collect all test cases in this class
+testcasesGetTransactionForRow = ["validRow", "invalidRow"]
+suiteGetTransactionForRow = unittest.TestSuite(map(getTransactionForRow, testcasesGetTransactionForRow))
+
+##########################################################
+
 # Collect all test cases in this file
-suites = [suiteGetHorizontalHeader]
+suites = [suiteGetHorizontalHeader, suiteGetTransactionForRow]
 suite = unittest.TestSuite(suites)
 
 if __name__ == "__main__":
