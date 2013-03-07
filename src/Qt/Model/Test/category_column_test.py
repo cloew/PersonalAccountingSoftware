d6a5d3628e4b676237cf31a4760b8bc3af0b8371
Chris Loew <cloew123@gmail.com>
2013-03-07 13:01:59 -0700

    Added test of the category column. Setting data does not work.

---
+++ src/Qt/Model/Test/category_column_test.py
@@ -0,0 +1,66 @@
+from db.categories import Categories
+from ORM.category import Category
+from ORM.transaction import Transaction
+from PyQt4.QtCore import QVariant
+from Qt.Model.category_column import CategoryColumn
+
+import unittest
+
+class getDataForTransaction(unittest.TestCase):
+    """ Test cases of getDataForTransaction """
+    
+    def  setUp(self):
+        """ Build the *** for the test """
+        self.transaction = Transaction()
+        self.categoryColumn = CategoryColumn()
+        self.name = "Category"
+        self.category = Category(name=self.name)
+        
+    def noCategory(self):
+        """ Test that getDataForTransaction properly handles when there is no category """
+        self.transaction.category = None
+        data = self.categoryColumn.getDataForTransaction(self.transaction)
+        assert data is None, "Should get no data when the transaction category is None"
+
+    def aCategory(self):
+        """ Test that getDataForTransaction properly handles when there is a category """
+        self.transaction.category = self.category
+        data = self.categoryColumn.getDataForTransaction(self.transaction)
+        assert str(data.toString()) == self.name, "Should get the category name as the data"
+
+# Collect all test cases in this class
+testcasesGetDataForTransaction = ["noCategory", "aCategory"]
+suiteGetDataForTransaction = unittest.TestSuite(map(getDataForTransaction, testcasesGetDataForTransaction))
+
+##########################################################
+
+class setDataForTransaction(unittest.TestCase):
+    """ Test cases of setDataForTransaction """
+    
+    def  setUp(self):
+        """ Build the *** for the test """
+        self.transaction = Transaction()
+        self.transaction.category = None
+        self.categoryColumn = CategoryColumn()
+
+    def categoryString(self):
+        """ Test that setDataForTransaction properly handles when there is a category """
+        name = Categories.all()[0]
+        category = "Some Category"
+        value = QVariant(name)
+        dataSet = self.categoryColumn.setDataForTransaction(self.transaction, value)
+        assert dataSet, "Should have data set"
+        assert self.transaction.category.name == name, "Should have the Transaction Category"
+
+# Collect all test cases in this class
+testcasesSetDataForTransaction = ["categoryString"]
+suiteSetDataForTransaction = unittest.TestSuite(map(setDataForTransaction, testcasesSetDataForTransaction))
+
+##########################################################
+
+# Collect all test cases in this file
+suites = [suiteGetDataForTransaction, suiteSetDataForTransaction]
+suite = unittest.TestSuite(suites)
+
+if __name__ == "__main__":
+    unittest.main()
--- src/Qt/Model/Test/suite.py
+++ src/Qt/Model/Test/suite.py
@@ -1,4 +1,5 @@
 from amount_column_test import suite as amount_column_suite
+from category_column_test import suite as category_column_suite
 from date_column_test import suite as date_column_suite
 from description_column_test import suite as description_column_suite
 from transaction_column_test import suite as transaction_column_suite
@@ -7,6 +8,7 @@ from type_column_test import suite as type_column_suite
 import unittest
 
 suites = [amount_column_suite,
+          category_column_suite,
           date_column_suite,
           description_column_suite,
           transaction_column_suite,
