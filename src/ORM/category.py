c3a27fca15f9f99b91c8f80c2d222df831476971
Chris Loew <cloew123@gmail.com>
2013-03-07 08:52:35 -0700

    Added Category ORM Class

---
+++ src/ORM/category.py
@@ -0,0 +1,13 @@
+from orm_base import Base
+from sqlalchemy import Column, String
+
+class Category(Base):
+    """ Represents a Financial Transaction. """
+    __tablename__ = 'categories'
+
+    # Database Columns
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
+
+    def __repr__(self):
+        return "<Category('{0}')>".format(self.name)
