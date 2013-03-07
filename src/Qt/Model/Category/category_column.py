5530fc62ff44a997ef1a4a26a05135c1ed07d2c3
Chris Loew <cloew123@gmail.com>
2013-03-07 16:57:07 -0700

    Added the category name column

---
+++ src/Qt/Model/Category/category_column.py
@@ -0,0 +1,41 @@
+from db.categories import Categories
+from PyQt4.QtCore import QAbstractTableModel, QModelIndex, QVariant, Qt
+
+class CategoryColumn:
+    """ Represents a column in the Categories Table """
+    header_name = "TODO: Replace with real name" # Should be overridden in Sub Class
+
+    def getHorizontalHeader(self):
+        """ Return Header Data """
+        return QVariant(self.header_name)
+
+    def getData(self, row):
+        """ Return data for the category in the given row """
+        category = self.getCategoryForRow(row)
+        if category is not None:
+            return self.getDataForCategory(category)
+
+    def getDataForCategory(self, category):
+        """ Return data for the provided category.
+            Should be overridden in sub class """
+
+    def setData(self, row, value):
+        """ Set the data in this column of the given category """
+        category = self.getCategoryForRow(row)
+        if category is not None:
+            return self.setDataForCategory(category, value)
+
+    def setDataForCategory(self, transaction, value):
+        """ Set data for the provided category.
+            Return whether the entry was set.
+            Should be overridden in sub class. """
+
+    def getTip(self, row):
+        """ Return the Status/Tool Tip for the given row.
+            Should be overridden in sub class. """
+
+    def getCategoryForRow(self, row):
+        """ Returns the Transaction in the given row """
+        categories = Categories.all()
+        if row < len(categories):
+            return categories[row]
--- src/Qt/Model/Category/category_table_model.py
+++ src/Qt/Model/Category/category_table_model.py
@@ -1,12 +1,14 @@
 from db.categories import Categories
 from Qt.Model.table_model import TableModel
 
+from name_column import NameColumn
+
 class CategoryTableModel(TableModel):
     """ Represnts the Category List as a Table """
 
     def getColumns(self):
         """ Get the Columns for the table """
-        return []
+        return [NameColumn()]
 
     def rowCount(self, parent):
         """ Returns the number of rows in the table """
