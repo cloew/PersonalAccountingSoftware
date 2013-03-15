968a6c917d322208227a5c15ef71711ce3f1f4d6
Chris Loew <cloew123@gmail.com>
2013-03-15 11:17:07 -0600

    Added logic so the Table can better manage it's number of rows

--- src/Qt/Model/Transaction/transaction_table_model.py
+++ src/Qt/Model/Transaction/transaction_table_model.py
@@ -7,6 +7,7 @@ from reconciled_column import ReconciledColumn
 from type_column import TypeColumn
 
 from db.transactions import Transactions
+from PySide.QtCore import Qt
 from Qt.Model.table_model import TableModel
 
 class TransactionTableModel(TableModel):
@@ -17,6 +18,7 @@ class TransactionTableModel(TableModel):
     def __init__(self):
         """ Initialize the Table Model """
         self.retriever = self.default_retriever
+        self.last_count = -1
         TableModel.__init__(self)
 
     def getColumns(self):
@@ -33,6 +35,13 @@ class TransactionTableModel(TableModel):
         """ Returns the number of rows in the table """
         return len(self.retriever())
 
+    def setData(self, index, value, role = Qt.EditRole):
+        """ Set Data in the Transaction Table """
+        dataSet = TableModel.setData(self, index, value, role)
+        if dataSet:
+            self.checkCount()
+        return dataSet
+
     def setTransactionRetriever(self, transactionFilter):
         """ Set the Transaction Retriever """
         if transactionFilter in self.transaction_retrievers:
@@ -40,3 +49,15 @@ class TransactionTableModel(TableModel):
 
             for column in self.columns:
                 column.transactionRetriever = self.retriever
+            self.checkCount()
+
+    def checkCount(self):
+        """ Check if the number of records in the table has changed """
+        count = self.rowCount(self)
+        if self.last_count != -1:
+            if count < self.last_count:
+                self.removeRows(count-1, self.last_count-count)
+            elif count > self.last_count:
+                self.insertRows(self.last_count-1, count-self.last_count)
+        self.last_count = count
+        return count
