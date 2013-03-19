37403366935d4ae7d1cf3d92a91ed3a6894097de
Chris Loew <cloew123@gmail.com>
2013-03-19 09:14:11 -0600

    Added account to the seed files

--- src/seed.py
+++ src/seed.py
@@ -18,22 +18,23 @@ def AddCategory(name=""):
     Categories.add(my_category)
     return my_category
 
-def AddTransaction(description="", amount=0, income=False, date=datetime.date.today(), cleared=None):
+def AddTransaction(description="", amount=0, income=False, date=datetime.date.today(), cleared=None, account=None):
     my_transaction = Transaction(description=description, amount=amount*100, income=income, date=date, cleared=cleared)
     Transactions.add(my_transaction)
+    my_transaction.account = account
     return my_transaction
 
 def main():
-    AddAccount(name="Checking Account", balance=10000)
+    account = AddAccount(name="Checking Account", balance=10000)
 
     category = AddCategory(name="Gas")
     AddCategory(name="Groceries")
     AddCategory(name="Rent")
     AddCategory(name="Utilities")
 
-    AddTransaction(description="Dinner", amount=251.00, income=False, date=datetime.datetime.now())
-    transaction = AddTransaction(description="Gas", amount=35.00, income=False, date=datetime.date(2013, 3, 3))
-    AddTransaction(description="Paycheck", amount=1000.00, income=True, date=datetime.date(2013, 3, 4))
+    AddTransaction(description="Dinner", amount=251.00, income=False, date=datetime.datetime.now(), account=account)
+    transaction = AddTransaction(description="Gas", amount=35.00, income=False, date=datetime.date(2013, 3, 3), account=account)
+    AddTransaction(description="Paycheck", amount=1000.00, income=True, date=datetime.date(2013, 3, 4), account=account)
 
     transaction.category = category
     Transactions.save()
