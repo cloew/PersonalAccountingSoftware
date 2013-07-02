6b6ae3c8608d29c2325ef65e7134bd021dac52ec
Chris Loew <cloew123@gmail.com>
2013-07-01 23:34:30 -0600

    Description Table Item can now save Description data

--- src/Qt/GUI/Transaction/Columns/description_table_item.py
+++ src/Qt/GUI/Transaction/Columns/description_table_item.py
@@ -1,3 +1,6 @@
+from db.transactions import Transactions
+
+from PySide.QtCore import Qt
 from PySide.QtGui import QTableWidgetItem
 
 class DescriptionTableItem(QTableWidgetItem):
@@ -7,3 +10,10 @@ class DescriptionTableItem(QTableWidgetItem):
         """ Initialize the Description Item """
         QTableWidgetItem.__init__(self, transaction.description)
         self.transaction = transaction
+        
+    def setData(self, role, value):
+        """ Set Data in Item """
+        if role == Qt.EditRole:
+            self.transaction.description = value
+            Transactions.save()
+        return QTableWidgetItem.setData(self, role, value)
