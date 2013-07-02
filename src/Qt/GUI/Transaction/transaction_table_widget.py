6caa49e37124a2b6e2868fce46fd6cef9fab9f4c
Chris Loew <cloew123@gmail.com>
2013-07-01 20:27:42 -0600

    Moved table population to its own function

--- src/Qt/GUI/Transaction/transaction_table_widget.py
+++ src/Qt/GUI/Transaction/transaction_table_widget.py
@@ -9,16 +9,20 @@ class TransactionTableWidget(QTableWidget):
     def __init__(self):
         """ Initialize the Transaction Table Widget """
         self.account = Accounts.all()[0]
-        transactions = Transactions.all()
-        columns = ["Amount", "Description", "Type", "Category", "Date", "Balance", "Cleared", "Reconciled"]
-        QTableWidget.__init__(self, len(transactions), len(columns))
+        self.columns = ["Amount", "Description", "Type", "Category", "Date", "Balance", "Cleared", "Reconciled"]
+        transactions = Transactions.allForAccount(self.account)
+        QTableWidget.__init__(self, len(transactions), len(self.columns))
         
-        self.setHorizontalHeaderLabels(columns)
+        self.setHorizontalHeaderLabels(self.columns)
         self.verticalHeader().hide()
         
+        self.populateTable(transactions)
+        
+    def populateTable(self, transactions):
+        """ Load Transactions from the Database """
         for row in range(len(transactions)):
-            for column in range(len(columns)):
-                item = QTableWidgetItem(str(row*len(columns)+column))
+            for column in range(len(self.columns)):
+                item = QTableWidgetItem(str(row*len(self.columns)+column))
                 self.setItem(row, column, item)
         
         
