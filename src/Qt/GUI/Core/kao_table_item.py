b6d489924854cddf175e942e94955440de7c4b5c
Chris Loew <cloew123@gmail.com>
2013-07-12 11:17:12 -0600

    Added a table item base class to be extended by the other table items

---
+++ src/Qt/GUI/Core/kao_table_item.py
@@ -0,0 +1,17 @@
+from PySide.QtCore import Qt
+from PySide.QtGui import QTableWidgetItem
+
+class KaoTableItem(QTableWidgetItem):
+    """ Base class for Kao Ttessur Qt Table Items """
+    
+    def __init__(self):
+        """ Initialize the Kao Table Item """
+        QTableWidgetItem.__init__(self, self.getData())
+        
+    def getData(self):
+        """ Return the item's data as a string """
+        return ""
+        
+    def updateData(self):
+        """ Update the Table Item's text and data """
+        self.setText(self.getData())
