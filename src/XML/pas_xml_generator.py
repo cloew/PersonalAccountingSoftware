aeb8e69d9589b8274fedc8b79e4964e9004957e9
Chris Loew <cloew123@gmail.com>
2013-08-13 17:47:59 -0600

    PAS XML generator now produces pretty XML

--- src/XML/pas_xml_generator.py
+++ src/XML/pas_xml_generator.py
@@ -1,7 +1,8 @@
 from db.accounts import Accounts
 from XML.account_element_factory import CreateAccountElement
 
-from xml.etree.ElementTree import Element, ElementTree, SubElement
+from xml.dom.minidom import parseString
+from xml.etree.ElementTree import Element, tostring, SubElement
 
 def Export():
     """ Export the PAS Database """
@@ -11,6 +12,14 @@ def Export():
     for account in Accounts.all():
         accountElement = CreateAccountElement(account)
         accountsElement.append(accountElement)
+      
+    SaveXMLFile(pasElement)
         
-    tree = ElementTree(pasElement)
-    tree.write("export.xml")
+def SaveXMLFile(root):
+    """ Save the XML from the given root into a file """ 
+    xmlString = tostring(root)
+    domXML = parseString(xmlString)
+    prettyXMLString = domXML.toprettyxml()
+    
+    with open("export.xml", 'w') as exportFile:
+        exportFile.write(prettyXMLString)
