b40754d149614a648db66a7ad02b42617c64a918
Chris Loew <cloew123@gmail.com>
2013-08-14 11:12:04 -0600

    Exporting Transactions now stores the matching account

--- src/XML/transaction_element_factory.py
+++ src/XML/transaction_element_factory.py
@@ -1,5 +1,12 @@
+from db.accounts import Accounts
+from db.categories import Categories
 from db.transactions import Transactions
+from db.transfers import Transfers
+from ORM.transaction import Transaction
+from ORM.transfer import Transfer
 from xml.etree.ElementTree import Element, SubElement
+
+from dateutil import parser
 
 def CreateTransactionElements():
     """ Create Transaction Elements to the parent element """
@@ -33,6 +40,10 @@ def CreateTransactionElement(transaction):
     reconciledElement = SubElement(transactionElement, "reconciled")
     reconciledElement.text = str(transaction.reconciled)
     
+    if transaction.account:
+        accountElement = SubElement(transactionElement, "account")
+        accountElement.text = transaction.account.name
+    
     if transaction.category:
         categoryElement = SubElement(transactionElement, "category")
         categoryElement.text = transaction.category.name
@@ -42,3 +53,42 @@ def CreateTransactionElement(transaction):
         transferElement.text = str(transaction.transferAccount.name)
     
     return transactionElement
+    
+def LoadTransactions(parentElement):
+    """ Load all transactions from the XML """
+    transactionsElement = parentElement.find("transactions")
+    for transactionElement in transactionsElement.findall("transaction"):
+        LoadTransaction(transactionElement)
+    
+def LoadTransaction(transactionElement):
+    """ Load a transaction from XML """
+    amount = int(transactionElement.findtext("amount"))
+    description = transactionElement.findtext("description")
+    income = StringToBoolean(transactionElement.findtext("income"))
+    date = parser.parse(transactionElement.findtext("date"))
+    cleared = StringToBoolean(transactionElement.findtext("cleared"))
+    reconciled = StringToBoolean(transactionElement.findtext("reconciled"))
+    accountName = transactionElement.findtext("account")
+    categoryName = transactionElement.findtext("category")
+    transferAccountName = transactionElement.findtext("transfer")
+    
+    transaction = Transaction(amount=amount, description=description, date=date, income=income, cleared=cleared, reconciled=reconciled)
+    
+    if accountName:
+        account = Accounts.accountWithName(accountName)
+        transaction.account = account
+    
+    if categoryName:
+        category = Categories.findByName(categoryName)
+        transaction.category = category
+        
+    if transferAccountName:
+        transferAccount = Accounts.accountWithName(transferAccountName)
+        transfer = Transfer(transaction=transaction, account=transferAccount)
+        Transfers.add(transfer)
+    
+    Transactions.add(transaction)
+    
+def StringToBoolean(someString):
+    """ Convert a String to a Boolean """
+    return someString == "True"
