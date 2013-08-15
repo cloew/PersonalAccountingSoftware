e10ae94acc779d1f58da4d0735396df4570ab199
Chris Loew <cloew123@gmail.com>
2013-08-15 10:43:58 -0600

    Tidied up Subtransaction so it actually works properly

--- src/ORM/subtransaction.py
+++ src/ORM/subtransaction.py
@@ -10,9 +10,9 @@ class SubTransaction(Base):
     # Database Columns
     id = Column(Integer, primary_key = True)
     transaction_id = Column(Integer, ForeignKey('transactions.id'))
-    transaction = relationship("Transaction", backref=backref('subtransaction'))
-    transaction_id = Column(Integer, ForeignKey('transactions.id'))
-    parent_transaction = relationship("Account", backref=backref('childTransactions'))
+    transaction = relationship("Transaction", foreign_keys=[transaction_id], backref=backref('subtransaction'))
+    parent_transaction_id = Column(Integer, ForeignKey('transactions.id'))
+    parent_transaction = relationship("Transaction", foreign_keys=[parent_transaction_id], backref=backref('childTransactions'))
 
     def __repr__(self):
         return "<SubTransaction('{0}' is related to '{1}')>".format(self.transaction, self.parent_transaction)
