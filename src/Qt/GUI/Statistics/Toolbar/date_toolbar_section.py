70e1e57ce20f19b9c9a5a8fac6928de9a72f44b9
Chris Loew <cloew123@gmail.com>
2013-08-15 16:19:48 -0600

    Added Date Toolbar Section

---
+++ src/Qt/GUI/Statistics/Toolbar/date_toolbar_section.py
@@ -0,0 +1,36 @@
+from PySide.QtGui import QComboBox, QLabel
+
+class DateToolbarSection:
+    """ Represents the Date Toolbar Section """
+    
+    def __init__(self, toolbar, panel):
+        """ Initialize the Date Toolbar Section """
+        self.toolbar = toolbar
+        self.panel = panel
+        
+    def addDateSection(self):
+        """ Add Date Section """
+        self.dateLabel = QLabel("Month: ", self.toolbar)
+        self.toolbar.addWidget(self.dateLabel)
+        self.monthComboBox = QComboBox(self.toolbar)
+        self.monthComboBox.addItems(["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"])
+        self.monthComboBox.activated.connect(self.setMonth)
+        self.toolbar.addWidget(self.monthComboBox)
+        
+        self.yearComboBox = QComboBox(self.toolbar)
+        self.yearComboBox.addItems(["2013"])
+        self.yearComboBox.activated.connect(self.setYear)
+        self.toolbar.addWidget(self.yearComboBox)
+    
+    def setMonth(self, index):
+        """ Set the month """
+        categoryStatistics = self.getCategoryStatistics()
+        categoryStatistics.month = index+1
+        self.panel.updatePanel()
+    
+    def setYear(self):
+        """ Set the year """
+        
+    def getCategoryStatistics(self):
+        """ Returns the Category Statistics """
+        return self.panel.categoryStatistics
