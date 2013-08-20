34c8873e5f60f803a71d7de89dd64fcc5a0fb554
Chris Loew <cloew123@gmail.com>
2013-08-20 10:53:27 -0600

    Added Date Helper

---
+++ src/Utilities/date_helper.py
@@ -0,0 +1,9 @@
+from dateutil import parser
+
+def DateToString(date):
+    """ Returns a string format of the given date """
+    return "{0:%m/%d/%Y}".format(self.initial_balance_date)
+    
+def StringToDate(dateString):
+    """ Converts a String to a date """
+    return parser.parse(value)
