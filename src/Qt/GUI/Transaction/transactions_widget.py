670874d36d33102d1c401dd34423eb07a4c22082
Chris Loew <cloew123@gmail.com>
2013-09-23 13:45:50 -0600

    Tidied up the Transactions widget

--- src/Qt/GUI/Transaction/transactions_widget.py
+++ src/Qt/GUI/Transaction/transactions_widget.py
@@ -2,7 +2,7 @@ from Qt.GUI.Transaction.Table.transaction_table_widget import TransactionTableWi
 from Qt.GUI.Transaction.Menu.transaction_menu_widget import TransactionMenuWidget
 
 from PySide.QtCore import Qt
-from PySide.QtGui import QDockWidget, QHBoxLayout, QFrame, QWidget
+from PySide.QtGui import QHBoxLayout, QFrame, QWidget
 
 class TransactionsWidget(QFrame):
     """ Represents the Widget that holds the Transaction table and menu """
@@ -10,14 +10,9 @@ class TransactionsWidget(QFrame):
     def __init__(self, parent):
         """ Initialize the Transactions Widget """
         QFrame.__init__(self)
-        someWidget = QWidget(self)
-        # self.dockWidget = QDockWidget("Transaction Details")
         
         self.transactionTableWidget = TransactionTableWidget()
         self.transactionMenuWidget = TransactionMenuWidget(self)
-        # self.dockWidget.setWidget(self.transactionMenuWidget)
-        
-        #parent.addDockWidget(Qt.DockWidgetArea(Qt.RightDockWidgetArea), self.dockWidget)
         
         self.transactionMenuWidget.setMinimumSize(400, 800)
         self.transactionTableWidget.setMaximumSize(1200, 800)
