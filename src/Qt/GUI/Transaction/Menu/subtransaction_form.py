b36171cec4864cc789f38a1f411dea414bfc625e
Chris Loew <cloew123@gmail.com>
2013-09-23 17:31:42 -0600

    Moved the subtransaction logic to its own class

---
+++ src/Qt/GUI/Transaction/Menu/subtransaction_form.py
@@ -0,0 +1,60 @@
+from db.accounts import Accounts
+from db.transactions import Transactions
+from ORM.transaction import Transaction
+from Qt.GUI.Utilities.account_combobox_helper import UpdateComboBoxWithAccounts
+from Utilities.balance_helper import TheBalanceHelper
+
+from PySide.QtGui import QComboBox, QFormLayout, QFrame, QLabel, QLineEdit, QPushButton
+
+import datetime
+
+class SubtransactionForm:
+    """ Represents the SubTransaction Form """
+    
+    def __init__(self, parent):
+        """ Initialize the Subtransaction Form """
+        self.parent = parent
+    
+    def setup(self):
+        """ Setup the Subtransactions for the Transaction Details """
+        self.subtransactionFrame = QFrame()
+        formLayout = QFormLayout(self.subtransactionFrame)
+        
+        label = QLabel()
+        label.setText("<b>Subtransactions</b>")
+        formLayout.addRow(label)
+        self.accountComboBox = QComboBox()
+        UpdateComboBoxWithAccounts(self.accountComboBox)
+        formLayout.addRow("Account:", self.accountComboBox)
+        formLayout.addRow("Amount:", QLineEdit())
+        button = QPushButton("Add New Subtransaction")
+        button.clicked.connect(self.saveTransaction)
+        formLayout.addRow(button)
+        self.subtransactionFrame.setLayout(formLayout)
+        self.layout.addWidget(self.subtransactionFrame)
+        
+    def tabSelected(self):
+        """ Update the Account Tab when the tab is selected """
+        text = self.accountComboBox.currentText()
+        UpdateComboBoxWithAccounts(self.accountComboBox)
+        index = self.accountComboBox.findText(text)
+        if not (index == -1):
+            self.accountComboBox.setCurrentIndex(index)
+            
+    def saveTransaction(self, checked=False):
+        """ Save the current Transaction """
+        transaction = Transaction(date=datetime.date.today())
+        account = Accounts.all()[self.accountComboBox.currentIndex()]
+        transaction.account = account
+        Transactions.add(transaction)
+        TheBalanceHelper.setupBalancesForAccount(transaction.account)
+        #self.table_view.insertRow(transaction)
+        
+    
+    @property
+    def layout(self):
+        return self.parent.layout
+        
+    @property
+    def transaction(self):
+        return self.parent.transaction
--- src/Qt/GUI/Transaction/Menu/transaction_menu_widget.py
+++ src/Qt/GUI/Transaction/Menu/transaction_menu_widget.py
@@ -1,12 +1,6 @@
-from db.accounts import Accounts
-from db.transactions import Transactions
-from ORM.transaction import Transaction
-from Qt.GUI.Utilities.account_combobox_helper import UpdateComboBoxWithAccounts
-from Utilities.balance_helper import TheBalanceHelper
+from Qt.GUI.Transaction.Menu.subtransaction_form import SubtransactionForm
 
-from PySide.QtGui import QComboBox, QFormLayout, QFrame, QLabel, QLineEdit, QPushButton, QVBoxLayout
-
-import datetime
+from PySide.QtGui import QFrame, QLabel, QVBoxLayout
 
 class TransactionMenuWidget(QFrame):
     """ Represents Transaction Menu Widget """
@@ -14,13 +8,15 @@ class TransactionMenuWidget(QFrame):
     def __init__(self, parent=None):
         """ Initialize the Transaction Menu Widget """
         QFrame.__init__(self, parent=parent)
-        
-        self.transaction = None
-        
         self.setFrameShape(QFrame.Panel)
         self.layout = QVBoxLayout(self)
+        
+        self.transaction = None
+        self.forms = [SubtransactionForm(self)]
+        
         self.setupHeader()
-        self.setupMenuBody()
+        for form in self.forms:
+            form.setup()
         self.layout.addStretch()
         
     def setupHeader(self):
@@ -34,28 +30,6 @@ class TransactionMenuWidget(QFrame):
         self.transactionLabel = QLabel()
         self.setTransactionLabelText()
         self.layout.addWidget(self.transactionLabel)
-    
-    def setupMenuBody(self):
-        """ Setup the Body of the Menu """
-        self.setupSubtransactions()
-        
-    def setupSubtransactions(self):
-        """ Setup the Subtransactions for the Transaction Details """
-        self.subtransactionFrame = QFrame()
-        formLayout = QFormLayout(self.subtransactionFrame)
-        
-        label = QLabel()
-        label.setText("<b>Subtransactions</b>")
-        formLayout.addRow(label)
-        self.accountComboBox = QComboBox()
-        UpdateComboBoxWithAccounts(self.accountComboBox)
-        formLayout.addRow("Account:", self.accountComboBox)
-        formLayout.addRow("Amount:", QLineEdit())
-        button = QPushButton()
-        button.clicked.connect(self.saveTransaction)
-        formLayout.addRow(button)
-        self.subtransactionFrame.setLayout(formLayout)
-        self.layout.addWidget(self.subtransactionFrame)
         
     def updateOnTransactionChange(self, currentTransaction):
         """ Update menu contents when the current transaction changes Transaction Change """
@@ -69,18 +43,6 @@ class TransactionMenuWidget(QFrame):
             
     def tabSelected(self):
         """ Update the Account Tab when the tab is selected """
-        text = self.accountComboBox.currentText()
-        UpdateComboBoxWithAccounts(self.accountComboBox)
-        index = self.accountComboBox.findText(text)
-        if not (index == -1):
-            self.accountComboBox.setCurrentIndex(index)
-            
-    def saveTransaction(self, checked=False):
-        """ Save the current Transaction """
-        transaction = Transaction(date=datetime.date.today())
-        account = Accounts.all()[self.accountComboBox.currentIndex()]
-        transaction.account = account
-        Transactions.add(transaction)
-        TheBalanceHelper.setupBalancesForAccount(transaction.account)
-        #self.table_view.insertRow(transaction)
+        for form in self.forms:
+            form.tabSelected()
         
