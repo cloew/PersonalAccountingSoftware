f3063fb293b931d113bec831dc5996fbb795f126
Chris Loew <cloew123@gmail.com>
2013-09-29 21:55:19 -0600

    Added scripts to import and export locally

---
+++ src/export.py
@@ -0,0 +1,10 @@
+from XML.pas_xml_generator import Export
+
+import sys
+
+def main(args):
+    """ Run the main file """
+    Export()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ src/import.py
@@ -0,0 +1,10 @@
+from XML.pas_xml_generator import Import
+
+import sys
+
+def main(args):
+    """ Run the main file """
+    Import()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
