e286004dd74749b69beb4dac621fc2faf6a62f08
Chris Loew <cloew123@gmail.com>
2013-10-28 10:12:56 -0600

    Fixed some bad imports

--- src/Qt/GUI/Transaction/Table/transaction_table_widget.py
+++ src/Qt/GUI/Transaction/Table/transaction_table_widget.py
@@ -1,7 +1,7 @@
 from Qt.GUI.Core.kao_table_widget import KaoTableWidget
 
-from Qt.GUI.Transaction.Table.transaction_category_delegate import TransactionCategoryDelegate
-from Qt.GUI.Transaction.Table.transaction_type_delegate import TransactionTypeDelegate
+from Qt.GUI.Transaction.Table.Columns.Delegates.transaction_category_delegate import TransactionCategoryDelegate
+from Qt.GUI.Transaction.Table.Columns.Delegates.transaction_type_delegate import TransactionTypeDelegate
 
 from Qt.GUI.Transaction.Table.Columns.category_column import CategoryColumn
 from Qt.GUI.Transaction.Table.Columns.type_column import TypeColumn
