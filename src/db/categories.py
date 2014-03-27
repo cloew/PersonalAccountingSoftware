eb8c3430701bc5fc21f2dd290a533b8b08bd504d
Chris Loew <cloew123@gmail.com>
2014-03-26 18:14:12 -0600

    Categories sort in alphabetical order by default

--- src/db/categories.py
+++ src/db/categories.py
@@ -1,11 +1,20 @@
 from table_wrapper import TableWrapper
 from ORM.category import Category
 
+from sqlalchemy import asc
+from sqlalchemy.sql import collate
 
 class CategoriesWrapper(TableWrapper):
     """ Class to wrap interaction to the Categories table in the database """
     table_class = Category
 
+    def all(self, order=None):
+        """ Returns all transactions from the database """
+        if order is None:
+            return TableWrapper.all(self, order=collate(Category.name, 'NOCASE'))
+        else:
+            return TableWrapper.all(self, order=order)
+    
     def findByName(self, name):
         """ Return a Category with the given name """
         db_record = None
