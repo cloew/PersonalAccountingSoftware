65b890ea462d655e3336f18f1c652affe40f6fb9
Chris Loew <cloew123@gmail.com>
2015-05-13 18:19:32 -0600

    Added the displayable versions of the Initial balance and date to the account class

--- src/ORM/account.py
+++ src/ORM/account.py
@@ -2,8 +2,7 @@ from .orm_base import Base
 from Utilities.date_helper import DateToString
 from Utilities.dollar_amount_helper import GetDollarString
 
-
-from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String
+from sqlalchemy import Column, Date, Integer, String
 
 class Account(Base):
     """ Represents a Financial Account. """
@@ -14,6 +13,16 @@ class Account(Base):
     name = Column(String)
     initial_balance = Column(Integer) # In cents
     initial_balance_date = Column(Date)
+    
+    @property
+    def initialBalanceInDollars(self):
+        """ Return the initial balance in dollars """
+        return GetDollarString(self.initial_balance)
+    
+    @property
+    def initialBalanceDateString(self):
+        """ Return the initial balance in dollars """
+        return DateToString(self.initial_balance_date)
 
     def __repr__(self):
-        return "<Account('{0}', '{1}' as of {2})>".format(self.name, GetDollarString(self.initial_balance), DateToString(self.initial_balance_date))
+        return "<Account('{0}', '{1}' as of {2})>".format(self.name, self.initialBalanceInDollars, self.initialBalanceDateString)
